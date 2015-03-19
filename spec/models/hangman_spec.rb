41efa29693d1806c561078a78efed8f0dfadf130
matthew gould <gouldm44@gmail.com>
2015-03-19 16:52:40 -0400

    More working tests

--- spec/models/hangman_spec.rb
+++ spec/models/hangman_spec.rb
@@ -40,4 +40,28 @@ RSpec.describe Hangman, type: :model do
     expect(game.guesses).to eq ["x","t","r","b","n"]
     expect(game.board).to eq "b_n_n_"
   end
+
+  it "knows when the game is over" do
+    game = FactoryGirl.create :hangman
+
+    game.take_move "b"
+    game.take_move "a"
+    game.take_move "n"
+    
+    expect(game.won?).to eq true
+  end
+
+  it "ends the game when player runs out of turns" do
+    game = FactoryGirl.create :hangman
+
+    game.take_move "x"
+    game.take_move "t"
+    game.take_move "r"
+    game.take_move "e"
+    game.take_move "q"
+    game.take_move "w"
+
+    expect(game.lost?).to eq true
+    expect(game.guesses_left).to eq 0
+  end
 end
